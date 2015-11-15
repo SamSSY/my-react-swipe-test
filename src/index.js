33d9e7c2094a82e33889f1445cca779413d25b68
SamSSY <sam7822567@gmail.com>
2015-11-15 21:32:28 +0800

    1115/2134

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const React = require('react');
 const { render } = require('react-dom');
-const ReactSwipe = require('react-swipe');
+
+// material-ui
+const RaisedButton = require('material-ui/lib/raised-button');
+const Dialog = require('material-ui/lib/dialog');
+const ThemeManager = require('material-ui/lib/styles/theme-manager');
+const LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
+const Colors = require('material-ui/lib/styles/colors');
+const AppBar = require('material-ui/lib/app-bar');
+
+const injectTapEventPlugin = require('react-tap-event-plugin');
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 
 require('./main.scss');
 
@@ -14,15 +30,16 @@ const initialState = {
 		{content: 'six'},
 		{content: 'seven'},
 		{content: 'eight'}
-	]
+	],
+    muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
 }
 
 class SwipePane extends React.Component{
 	
     componentDidMount(){
-   
-        $( ".pane" ).on( "swiperight", swipeRightHandler );
-        $( ".pane" ).on( "swipeleft", swipeLeftHandler );
+        
+        $( ".pane" ).hammer().on( "swiperight", swipeRightHandler );
+        $( ".pane" ).hammer().on( "swipeleft", swipeLeftHandler );
 
         function swipeRightHandler( event ){
             
@@ -58,6 +75,22 @@ class SwipeBody extends React.Component {
         this.state= initialState;
     }
 
+    getChildContext() {
+        return {
+          muiTheme: this.state.muiTheme,
+        };
+    }
+
+    componentWillMount() {
+        //console.log("!!!");
+
+        let newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
+            accent1Color: Colors.deepOrange500,
+        });
+
+        this.setState({muiTheme: newMuiTheme});
+    }
+
     renderSwipePane(content, index){
     	return(
     		<SwipePane key={index} 
@@ -68,14 +101,25 @@ class SwipeBody extends React.Component {
 
     render() {
     	const contents = this.state.contents;
-        return ( 
-        	<div> 
-        		{contents.map(this.renderSwipePane, this)}
-        	</div>
+        console.log(this.state.muiTheme);
+
+        return (
+            <div>
+                <AppBar
+                  title="Title"
+                  iconClassNameRight="muidocs-icon-navigation-expand-more" /> 
+                  <RaisedButton label="Default" />
+            	<div> 
+            		{contents.map(this.renderSwipePane, this)}
+            	</div>
+            </div>
         );
     }
 }
 
+SwipeBody.childContextTypes = {
+    muiTheme: React.PropTypes.object,
+}
 
 
 render(<SwipeBody />, document.getElementById('root'));
